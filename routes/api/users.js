c3d250f71184197afc1dde912eef03b883ba4b18
Border Collie <koczfix@gmail.com>
2019-09-01 23:02:23 +0200

    Models update

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,9 +2,24 @@ const express = require("express");
 const router = express.Router();
 const { check, validationResult } = require("express-validator/check");
 
-router.post("/", (req, res) => {
-  console.log(req.body);
-  res.send("user route");
-});
+router.post(
+  "/",
+  [
+    check("name", "Imię jest wymagane")
+      .not()
+      .isEmpty(),
+    check("email", "Wprowadz email").isEmail(),
+    check("password", "Hasło powinno zawierać więcej niż 6 znaków").isLength({
+      min: 6
+    })
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    res.send("user route");
+  }
+);
 
 module.exports = router;
