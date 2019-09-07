6a5f30bcfe1f53bd07558f717056c396deeede4c
Border Collie <koczfix@gmail.com>
2019-09-07 22:32:27 +0200

    Login works

---
+++ middleware/auth.js
@@ -0,0 +1,23 @@
+const jwt = require("jsonwebtoken");
+const config = require("../config/default");
+
+module.exports = function(req, res, next) {
+  // odebranie tokena z header
+  const token = req.header("x-auth-token");
+
+  // sprawdzenie czy nie ma tokenu
+
+  if (!token) {
+    return res.status(401).json({ msg: "Brak tokenu" });
+  }
+
+  //weryfikacja tokenu
+  try {
+    const decoded = jwt.verify(token, config.options.jwtSecret);
+
+    req.user = decoded.user;
+    next();
+  } catch (err) {
+    res.status(401).json({ msg: "Token nie dziala" });
+  }
+};
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,9 +1,74 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../../middleware/auth");
+const { check, validationResult } = require("express-validator");
+const config = require("../../config/default");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
 
-router.post("/", (req, res) => {
-  console.log(req.body);
-  res.send("user route");
+const User = require("../../models/User");
+
+router.get("/", auth, async (req, res) => {
+  try {
+    console.log(User);
+    const user = await User.findByPk(req.user.id);
+    res.json(user);
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send("err");
+  }
 });
+router.post(
+  "/",
+  [
+    check("email", "Wprowadz email").isEmail(),
+    check("password", "haslo jest wymagane").exists()
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+
+    try {
+      let user = await User.findOne({
+        where: { email }
+      });
+      //Czy istnieje
+      if (!user) {
+        return res.status(400).json({ errors: [{ msg: "Bledne dane" }] });
+      }
+
+      const isMatch = await bcrypt.compare(password, user.password);
+      console.log(user);
+      if (!isMatch) {
+        return res.status(400).json({ errors: [{ msg: "Bledne dane 1" }] });
+      }
+
+      // Return JWT
+
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+
+      jwt.sign(
+        payload,
+        config.options.jwtSecret,
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
 
 module.exports = router;
